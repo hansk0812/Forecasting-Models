66ba5636734512670a78bf1fa232422bcb35cb3a
hanskrupakar <hans94812@gmail.com>
2025-06-22 09:32:12 +0530

    xlstm-params-2

--- models/xLSTM_TS.py
+++ models/xLSTM_TS.py
@@ -43,7 +43,7 @@ def create_xlstm_model(config):
                 embedding_dim=config.d_model, #RECURRENT_FACTOR*input_size,
                 proj_factor=2.,
                 channel_mixing=config.d_ff>0,
-                strided_conv=config.modes>0
+                strided_conv=config.modes
                 )
             )
     slstm_block=sLSTMBlockConfig(
@@ -54,8 +54,8 @@ def create_xlstm_model(config):
                 bias_init="powerlaw_blockdependent",
                 dropout=config.dropout,
                 embedding_dim=config.d_model,
-                channel_mixing=False,
-                strided_conv=True
+                channel_mixing=config.d_ff>0,
+                strided_conv=config.modes
                 ),
                 feedforward=FeedForwardConfig(proj_factor=1.1, act_fn="gelu"), # Reduced projection factor to save memory
             )
