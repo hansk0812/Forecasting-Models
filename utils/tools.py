15dc90f0b3e015f84f196f7e632112d2dbe063d0
hanskrupakar <hans94812@gmail.com>
2025-09-11 05:50:07 +0530

    lr-decay-schedule

--- utils/tools.py
+++ utils/tools.py
@@ -8,7 +8,10 @@ plt.switch_backend('agg')
 def adjust_learning_rate(optimizer, epoch, args):
     # lr = args.learning_rate * (0.2 ** (epoch // 2))
     if args.lradj == 'type1':
-        lr_adjust = {epoch: args.learning_rate * (0.5 ** ((epoch - 1) // 1))}
+        if args.model in ["NBEATS", "NHITS", "FiLM", "CycleNet", "NLinear", "DLinear", "TiDE"]:
+            lr_adjust = {epoch: args.learning_rate - args.learning_rate * (0.5 ** ((epoch) // 1))}
+        else:
+            lr_adjust = {epoch: args.learning_rate * (0.5 ** ((epoch-1) // 1))}
     elif args.lradj == 'type2':
         lr_adjust = {
             2: 5e-5, 4: 1e-5, 6: 5e-6, 8: 1e-6,
