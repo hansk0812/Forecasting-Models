85cf0aea28cde34fc798635af3c40801363c8aa6
Hans <hans94812@gmail.com>
2026-05-04 12:19:56 +0530

    added-yf-dataset

---
+++ dataset/YahooFinance/download.py
@@ -0,0 +1,18 @@
+import os
+import yfinance as yf
+
+"""
+AAPL Apple Inc. MCD McDonald’s Corporation
+ABT Abbott Laboratories MSFT Microsoft Corporation
+AEM Agnico Eagle Mines Limited ORCL Oracle Corporation
+AFG American Financial Group, Inc. WWD Woodward, Inc.
+APA Apache Corporation T AT&T Inc.
+CAT Caterpillar Inc. UTX --> RTX United Technologies Corporation
+"""
+stocks = ["AAPL", "MCD", "ABT", "MSFT", "AEM", "ORCL", 
+          "AFG", "WWD", "APA", "T", "CAT", "RTX"]
+
+for stock in stocks:
+    if not os.path.exists("./%s.csv" % stock):
+        data = yf.download(stock, period="10y")
+        data.to_csv("./%s.csv" % stock)
